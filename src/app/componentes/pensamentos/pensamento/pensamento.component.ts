d558d0601c279e4587bbd92bdbbb94b13b560312
Julia Oliveira <juliabomfimdeoliveira@hotmail.com>
2023-02-27 01:29:12 -0300

    Directives

---
+++ src/app/componentes/pensamentos/pensamento/pensamento.component.ts
@@ -0,0 +1,23 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-pensamento',
+  templateUrl: './pensamento.component.html',
+  styleUrls: ['./pensamento.component.css'],
+})
+export class PensamentoComponent {
+  @Input() pensamento = {
+    conteudo: 'I Love Angular',
+    autoria: 'Julia',
+    modelo: 'modelo3',
+  };
+
+  ngOnInit(): void {}
+
+  larguraPensamento(): string {
+    if (this.pensamento.conteudo.length >= 256) {
+      return 'pensamento-g';
+    }
+    return 'pensamento-p';
+  }
+}
